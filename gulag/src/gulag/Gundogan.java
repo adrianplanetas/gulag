e4519d13fd92121e8bf01287735a688a78aa3ec0
adrianplanetas <dw1acovid@gmail.com>
2021-05-20 19:46:12 +0100

    inicio

---
+++ gulag/src/gulag/Gundogan.java
@@ -0,0 +1,13 @@
+package gulag;
+
+public class Gundogan {
+	public boolean visualizarSalida(boolean acertada, int i, String newParam) {
+		return acertada;
+		
+		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
